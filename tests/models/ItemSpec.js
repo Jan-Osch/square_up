20cc394c9f837caf1c1590e00b5753a204450277
janek_g <jmgrzesik@gmail.com>
2015-10-04 20:07:00 +0200

    Adds simple test case for Item

--- tests/models/ItemSpec.js
+++ tests/models/ItemSpec.js
@@ -2,10 +2,24 @@ describe('Item' ,function(){
    var Item,
        item;
 
+    beforeEach(function(){
+        item = undefined;
+    });
+
     it('is defined as a package', function(){
         Item  = require('.././models/Item.js').Item;
         expect(Item).toBeDefined();
-    })
+    });
 
-    it('')
+    it('new instance is created and uuid added', function(){
+        var mockName = 'name';
+        var mockPrice = 100;
+        var mockIdentitesPayed = ['A', 'B'];
+        var mockIdentitesToPay = ['A', 'B'];
+        var mockValuesPaid = {
+            'A' : 80,
+            'B' : 20
+        };
+        item = new Item()
+    })
 });
