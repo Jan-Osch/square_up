a8e3ad485d6cec764eb83961610ee1145583256c
janek_g <jmgrzesik@gmail.com>
2015-10-04 19:34:42 +0200

    Adds a test suite for Item, resolves problems with imports

--- tests/RoundingHelperSpec.js
+++ tests/RoundingHelperSpec.js
@@ -1,27 +1,27 @@
 describe('RoundingHelper', function () {
     var round = require('../lib/RoundingHelper.js').round;
     it('function round is defined', function () {
-       expect(round).toBeDefined();
+        expect(round).toBeDefined();
     });
-    it('rounds small values as expected', function(){
+    it('rounds small values as expected', function () {
         var value = 0.1;
         var result = round(value);
         var expected = 0;
         expect(result).toEqual(expected);
     });
-    it('rounds small values with a lot of digits', function(){
+    it('rounds small values with a lot of digits', function () {
         var value = 0.1123123;
         var result = round(value);
         var expected = 0;
         expect(result).toEqual(expected);
     });
-    it('rounds small values near 0.5', function(){
+    it('rounds small values near 0.5', function () {
         var value = 0.4912;
         var result = round(value);
         var expected = 0;
         expect(result).toEqual(expected);
-    })
-    it('rounds small values above 0.5', function(){
+    });
+    it('rounds small values above 0.5', function () {
         var value = 0.50003131;
         var result = round(value);
         var expected = 1;
---
+++ tests/models/ItemSpec.js
@@ -0,0 +1,11 @@
+describe('Item' ,function(){
+   var Item,
+       item;
+
+    it('is defined as a package', function(){
+        Item  = require('../../lib/models/Item.js').Item;
+        expect(Item).toBeDefined();
+    })
+
+    it('')
+});
